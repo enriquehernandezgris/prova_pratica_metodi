9a50b725fecc58c63e70fc76bccc8aab3911519a
ProCoder99 <maanuu30903@gmail.com>
2026-05-06 20:07:21 +0200

    Developed IntegerToRoman class.

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -3,8 +3,21 @@ package it.unipd.mtss;
 public class IntegerToRoman {
 
     public static String convert(int number) {
+        if (number < 1 || number > 1000) {
+            System.out.println("Il numero deve essere compreso tra 1 e 1000");
+            return null;
+        } else {
+            String[] letters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+            int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
-        // TODO 
-        return null;
+            StringBuilder romanNumber = new StringBuilder();
+            for (int i = 0; i < values.length; i++) {
+                while (number >= values[i]) {
+                    number -= values[i];
+                    romanNumber.append(letters[i]);
+                }
+            }
+            return romanNumber.toString();
+        }
     }
 }
