c7e74f80547d221a50d0e278e45aff47e028da26
ProCoder99 <maanuu30903@gmail.com>
2026-05-07 12:34:27 +0200

    Repaired checkstyle issues (Long line: line 15 and 16).

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -7,8 +7,10 @@ public class IntegerToRoman {
             System.out.println("Il numero deve essere compreso tra 1 e 1000");
             return null;
         } else {
-            String[] letters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-            int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+            String[] letters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X"
+                    , "IX", "V", "IV", "I"};
+            int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4,
+                    1};
 
             StringBuilder romanNumber = new StringBuilder();
             for (int i = 0; i < values.length; i++) {
