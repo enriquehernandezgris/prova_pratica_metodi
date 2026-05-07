f5611d0040c9f780ee90b9f3129f385792968a7b
ProCoder99 <maanuu30903@gmail.com>
2026-05-07 19:16:57 +0200

    Test from 1 to 3 and inferior limits.

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -5,6 +5,40 @@
 
 package it.unipd.mtss;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
 public class IntegerToRomanTest {
 
+    @Test
+    public void convert_NumbersOneToThree_ReturnCorrectRomanSymbols() {
+        assertEquals("I", IntegerToRoman.convert(1), "Fail converting 1");
+
+        assertEquals("II", IntegerToRoman.convert(2), "Fail converting 2");
+
+        assertEquals("III", IntegerToRoman.convert(3), "Fail converting 3");
+    }
+
+
+    // Errore limite inferiore
+    @Test
+    public void convert_NumberZero_ThrowsIllegalArgumentException() {
+        int input = 0;
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            IntegerToRoman.convert(input);
+        }, "Fail inferior limit");
+    }
+
+    // Errore limite negativo
+    @Test
+    public void convert_NegativeNumber_ThrowsIllegalArgumentException() {
+        int input = -5;
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            IntegerToRoman.convert(input);
+        }, "Fail negative limit");
+    }
 }
--- roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -5,6 +5,33 @@
 
 package it.unipd.mtss;
 
-public class RomanPrinterTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class RomanPrinterTest {
+    @Test
+    public void testPrint() {
+        assertEquals("""
+                 _____
+                |_   _|
+                  | |
+                  | |
+                 _| |_
+                |_____|""", RomanPrinter.print(1));
+        assertEquals("""
+                 _____   _____
+                |_   _| |_   _|
+                  | |     | |
+                  | |     | |
+                 _| |_   _| |_
+                |_____| |_____|""", RomanPrinter.print(2));
+        assertEquals("""
+                 _____   _____   _____
+                |_   _| |_   _| |_   _|
+                  | |     | |     | |
+                  | |     | |     | |
+                 _| |_   _| |_   _| |_
+                |_____| |_____| |_____|""", RomanPrinter.print(3));
+    }
 }
+
