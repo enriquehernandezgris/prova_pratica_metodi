56807b44e0bbea7e323322b8276af8684325a6f5
enriquehernandezgris <quiquepro1691@gmail.com>
2026-05-07 13:50:03 +0200

    updated App.java and AppTest.java (if its not in the folder, coveralls fails)

---
+++ roman-number/src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////
+// Enrique Hernández Gris 2169844
+// Manuel Ibáñez Martínez-Lozano 2169871
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Roman Number Converter");
+    }
+}
